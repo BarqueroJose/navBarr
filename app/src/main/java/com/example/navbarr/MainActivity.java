ec4769710892779eb176fbd9d13660600f88511a
barqu <josebarquerolazo12@gmail.com>
2023-09-20 11:18:12 -0600

    subida

---
+++ app/src/main/java/com/example/navbarr/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.navbarr;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+   PrimerFragmento primerFragmento = new PrimerFragmento();
+   SegundoFragmento segundoFragmento = new SegundoFragmento();
+   TercerFragmento tercerFragmento = new TercerFragmento();
+   CuartoFragmento cuartoFragmento = new CuartoFragmento();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        loadFragment(primerFragmento);
+    }
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.primerFragmento:
+                   loadFragment(primerFragmento);
+                    return true;
+                case R.id.segundoFragmento:
+                    loadFragment(segundoFragmento);
+                    return true;
+                case R.id.tercerFragmento:
+                    loadFragment(tercerFragmento);
+                    return true;
+                case R.id.cuartoFragmento:
+                    loadFragment(cuartoFragmento);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+    private void loadFragment(Fragment fragment){
+        FragmentTransaction transaction =  getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container,fragment);
+        transaction.commit();
+
+    }
+}
+
